d172c0c84b3b0602fc45c52f10a946ac43927245
Sudipa22 <50295564+Sudipa22@users.noreply.github.com>
2019-09-30 13:23:34 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,12 +1,13 @@
-def call(gitlab,branch_name) {
- withCredentials([string(credentialsId: 'gitlaburl', variable: 'gitlab')]) {
-                echo "My password is '${gitlab}'!"
-                checkout([$class: 'GitSCM',
-                branches: [[name: 'origin/master']],
-                extensions: [[$class: 'WipeWorkspace']],
-                userRemoteConfigs: [[url: "${gitlab}"]]
+def call(git_url,branch_name) {
+ echo "My git url is ${git_url}"
+     echo "Checkout from branch ${branch_name}"
+     checkout([$class: 'GitSCM',
+                 branches: [[name: "origin/${branch_name}"]],
+                 extensions: [[$class: 'WipeWorkspace']],
+                 userRemoteConfigs: [[url: "${git_url}"]]
+
                 ])
-                }
+                
 }
 
 
